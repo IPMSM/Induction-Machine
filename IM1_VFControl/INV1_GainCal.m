9aa434d03f971115971baa437fcfa982ac4edbb3
Hyeonjun Lee <lhjoon14@gmail.com>
2022-02-20 23:31:02 +0900

    1

---
+++ IM1_VFControl/INV1_GainCal.m
@@ -0,0 +1,3 @@
+%% V/F Controller
+IM.tacc = 1; %[sec]
+IM.acc_rate = IM.Wr_rated/IM.tacc;
--- IM1_VFControl/Induction_motor_parameter.m
+++ IM1_VFControl/Induction_motor_parameter.m
@@ -1,45 +1,39 @@
-clear all
-clc
-%% 3H 4Pole 220V rms Induction Motor
-Fs = 60;                %rated freqency motor [Hz]
-Ws = Fs*2*pi;           %rated angular frequency
-p = 4;                  %number of poles
-pp = p/2;               %number of pole paris
-P_rate = 3*746;         %rated Power
+%% Clear & DEFINE
+clear;
+clc;
+IM_VF_DC_Torque = 1;
+IM_VF = 1; IM_IndirectVector = 2;
+DC_Torque = 0; DC_Speed = 1; VFMode=1;
 
-Wrpm_rated = 1795;       %rated speed
-Wrm_rated = 2*pi*Wrpm_rated/60; 
-Wr_rated = pp*Wrm_rated;
-Vll = 220;              %rated Vline to line
-Vs_rated = Vll*sqrt(2) / sqrt(3); %Vs_rated phase voltage (peak)
-Jm = 0.089;			%intertia
-Bm = 0;
-%% Motor Parameters
-Rs = 0.435;		Rr = 0.816;
-Lls = 0.002;	Llr = 0.002;		Lm = 0.0693;
-Ls = Lm + Lls;	Lr = Lm + Llr;
-A = 1/(Ls*Lr-Lm*Lm);
-sigma = 1-Lm^2/(Ls*Lr);
-Kt = (3/2)*pp*(Lm/Lr);
-%% FOC 
-Pm_rated = 3*746;
-Te_rated = pp*Pm_rated/Wr_rated;
-Idse_rated = Vs_rated / sqrt(Rs^2 + (Ws*Ls)^2);
-Iqse_rated = Te_rated / ((3/2)*pp*(Lm^2/Lr)*Idse_rated);
-Lam_r_rated = Lm*Idse_rated;
-Lam_dre_rated = Lam_r_rated * 0.8;
+%% Operating Mode Variable %%
+TestMode = IM_VF_DC_Torque; %IM_VF_DC_Torque
 
-Vdc = 1.2*2*Vs_rated;
-%% Speed Controller
-Wsc = 2*pi*20;          %bandwidth
-Kps = Jm*Wsc;
-Kis = Kps*Wsc/5;
-Kas = 1/Kps;
-alpha = 1;              %PI Controller
-%% Current Controller
-Wcc = 10*Wsc;
-Kpc = sigma*Ls*Wcc;
-Kic = (Rs + Rr*(Lm/Lr)^2)*Wcc;
-Kac = 1/Kpc;
-sim('indmotorwithcc',2);
+% ------- Do not edit here ----------------- %
+if (TestMode  == IM_VF_DC_Torque)
+    INV1.DriveMode = IM_VF; %IM_VF
+    INV2.DriveMode = DC_Torque; % DC_Torque, DC_Speed
+end
+% ------------------------------------------ %
+
+%% Parameter Initialize
+Vdc = 300;
+run("LoadMachine_Parameter_Init.m");
+run("TestMachine_Parameter_Init.m");
+
+%% INV1 (Induction Machine) Refrence Inputs
+INV1.Speed_Ref = 1500; % RPM 
+INV1.Fsw = 10e3;
+INV1.Tsw = 1/INV1.Fsw;
+
+%% INV2 (DCMotor) Refrence Inputs
+INV2.Speed_Ref = 0 ;                    % Speed refrence [rev/min]
+INV2.Torque_Ref = 10;                  % Torque refrence [Nm]
+INV2.SwitchingFrequency = 10e3;    % Switching frequency [Hz]
+
+%% Inverter GainCal
+run("INV1_GainCal.m");
+run("INV2_GainCal.m");
+
+%% Run Simulation
+sim("IM1_VFControl.slx");
 
